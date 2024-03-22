cdf55bea17256201c1c8cd490859788c768f1be9
cvijayap <cvijayap@LIN-5CG204593H.corp.capgemini.com>
2024-03-22 14:30:32 +0530

    first commit

---
+++ src/main/java/com/demo1/Test.java
@@ -0,0 +1,16 @@
+package com.demo1;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+public class Test
+{
+	public static void main(String[] args) {
+			// TODO Auto-generated method stub
+			Resource r = new ClassPathResource("applicationContext.xml");
+			BeanFactory b = new XmlBeanFactory(r);
+			Employee e = (Employee) b.getBean("e");
+			e.show();
+		}
+}
